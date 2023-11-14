4c43b8be9dec31c8fb143e840c87fb51dfc9377f
Tadiparthi Sushma <tadiparthisushma@Tadiparthis-MacBook-Air.local>
2023-11-14 21:38:28 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+// components/HomePage.js
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Hotel Booking System</h1>
+      <p>Please log in to continue</p>
+      {/* Add your login form here */}
+    </div>
+  );
+}
+
+export default HomePage;
